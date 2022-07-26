1fee522ff52a4b865609a65ec2a1404de2994747
Justyn Enyart <gweeners17@gmail.com>
2022-07-26 17:44:56 -0400

    commit tree 905ada31d30cf7e08e0504eb909caf9bea9f484f

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if(word.split("").reverse().join("") === word) {
+    return true
+  } else if(word.split("").reverse().join("") !== word) {
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
+if string is palindrome
+  return true
+if string is not palindrome
+  return false
 */
 
 /*
-  Add written explanation of your solution here
+  split the string into an array, reverse the order of the array, rejoin the array into a string, 
+  compare the rejoined string to the original to see if it matches
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("carry"))
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
